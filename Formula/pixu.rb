3d97b237e604c419ab70fca79c750ae42ad988e3
pixu-ci <ci@xvoland.dev>
2026-09-04 14:21:42 +0000

    pixu v1.3.6

--- Formula/pixu.rb
+++ Formula/pixu.rb
@@ -1,9 +1,9 @@
 class Pixu < Formula
   desc "Terminal image viewer - render images directly in your terminal"
   homepage "https://github.com/xvoland/pixu"
-  url "https://github.com/xvoland/pixu/archive/refs/tags/v1.3.5.tar.gz"
-  sha256 "9c6a137c2c83841fd16a32eb39809e95042ad388055a7b0594f8c4378c20a043"
-  version "1.3.5"
+  url "https://github.com/xvoland/pixu/archive/refs/tags/v1.3.6.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  version "1.3.6"
   license "Apache-2.0"
 
   depends_on "go" => :build
